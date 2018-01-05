445922915051c554b49e0151ed9f52b722dc71f8
zhangtao <ztok999@163.com>
2018-01-05 08:18:39 +0800

    up

---
+++ js/common.js
@@ -0,0 +1,214 @@
+﻿var jQuery=$.noConflict();
+
+var newHeight = 0;//屏幕的高
+var newWidth = 0;//屏幕的宽
+var scrollChange = 0;//是否滚动
+//翻页
+var pageKey = 0;
+var pageSize = 3;
+
+//横竖屏监控
+function hengshuping(){  
+	if(window.orientation==180||window.orientation==0){        
+		//alert("竖屏状态！")          
+	}
+	if(window.orientation==90||window.orientation==-90){        
+		alert("请竖屏浏览！")            
+	} 
+}
+window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", hengshuping, false);
+
+jQuery(function(){
+
+	//自适应宽、高
+	newHeight = $(window).height();//得到屏幕的高
+	newWidth = $(window).width();//得到屏幕的宽
+	$(".win-height").css('height',newHeight);
+	$(".win-width").css('width',newWidth);
+	alert(newHeight)
+
+	
+	/*$(window).resize(function(){
+		newHeight = $(window).height();
+		$(".win-height").css('height',newHeight);
+		newWidth = $(window).width();
+		$(".win-width").css('width',newWidth);
+		
+	});
+	if(newHeight>450){//屏幕高度判断
+		$(".page1_left img").css('height','100%');
+		$(".page1_right img").css('height','100%');
+	}else{
+		$(".page1_left img").css('height','110%');
+		$(".page1_right img").css('height','110%');
+	}*/
+	page_one_animate();
+	page_bind();
+
+});
+
+
+//第一屏动画效果
+/*function page_one_animate(){
+	scrollChange = 0;
+	jQuery('.arrow').hide();
+	jQuery('.page1').show();
+	jQuery(".page1_left")
+		.stop()
+		.css('left' , '-100%')
+		.delay(100)//等待页面加载
+		.animate({
+			'left' : '0%',
+		},1500);
+	
+	jQuery(".page1_right")
+		.stop()
+		.css('right' , '-100%')
+		.delay(100)//等待页面加载
+		.animate({
+			'right' : '0%',
+		},1500)
+		.delay(2000)
+		.animate({
+			'right' : '0%',
+		},0,function(){
+			pageKey++;
+			customAnimate();
+		});
+}
+
+function initAnimate(){
+	jQuery(".arrow").hide();
+	scrollChange = 0;
+  if(pageKey == 0){//第一屏
+	  page_one_animate();
+  }
+  if(pageKey == 1){//第一屏
+	  
+	  jQuery(".arrow").show();
+  }
+
+}
+
+function customAnimate(){
+	if(pageKey >= pageSize) pageKey = pageSize - 1;
+	if(pageKey <= 0) pageKey = 0;
+	initAnimate();
+	thisTop = pageKey * newHeight + 'px';
+	jQuery("div#moveDiv").animate({'top':'-' +thisTop},10);
+	
+	if(pageKey == 1){
+		jQuery(".page2_1")
+		.delay(4000)
+		.animate({
+			'right' : '0%',
+		},0,function(){
+			pageKey++;
+			customAnimate();
+		});
+		
+	}
+	
+	if(pageKey == 2){
+		jQuery(window).scroll(function(){
+
+			if(jQuery(window).scrollTop() == 0 && scrollChange == 1){
+			
+				window.location.reload();
+			}else{
+			scrollChange = 1;
+			}
+		});
+	}
+	
+	
+}
+
+
+//绑定效果
+function page_bind(){
+	//绑定箭头
+	jQuery(".arrow").bind('click',function(){
+		pageKey++;
+		//scrollChange = 0;
+		customAnimate();
+	});
+	
+	//箭头抖动
+	setInterval(function(){
+		arrow_bottom = parseInt(jQuery(".arrow").css('bottom'));
+		if(arrow_bottom ==0){
+			arrow_bottom = 2;
+		}else{
+			arrow_bottom =0;
+		}
+				
+		jQuery(".arrow").css('bottom',arrow_bottom+'px');
+	},200);
+	
+}
+
+//微信分享用 开始
+var descContent = "";
+var shareTitle = "组 · 合---云、移动、物联网高峰论坛";
+var appid = '';
+var rootUrl = "http://114.112.98.110/web_group";
+
+function shareFriend() {
+    var imgUrl = rootUrl + '/images/share.jpg';
+    var lineLink = rootUrl;
+    WeixinJSBridge.invoke('sendAppMessage',{
+                            "appid": appid,
+                            "img_url": imgUrl,
+                            "img_width": "640",
+                            "img_height": "640",
+                            "link": lineLink,
+                            "desc": descContent,
+                            "title": shareTitle
+                            }, function(res) {
+                            _report('send_msg', res.err_msg);
+                            });
+}
+function shareTimeline() {
+		var imgUrl = rootUrl + '/images/share.jpg';
+    var lineLink = rootUrl;
+    WeixinJSBridge.invoke('shareTimeline',{
+                            "img_url": imgUrl,
+                            "img_width": "640",
+                            "img_height": "640",
+                            "link": lineLink,
+                            "desc": descContent,
+                            "title": shareTitle
+                            }, function(res) {
+                            _report('timeline', res.err_msg);
+                            });
+}
+function shareWeibo() {
+		var imgUrl = rootUrl + '/images/share.jpg';
+    var lineLink = rootUrl;
+    WeixinJSBridge.invoke('shareWeibo',{
+                            "content": descContent,
+                            "url": lineLink,
+                            }, function(res) {
+                            _report('weibo', res.err_msg);
+                            });
+}
+// 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
+document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+
+        // 发送给好友
+        WeixinJSBridge.on('menu:share:appmessage', function(argv){
+            shareFriend();
+            });
+
+        // 分享到朋友圈
+        WeixinJSBridge.on('menu:share:timeline', function(argv){
+            shareTimeline();
+            });
+
+        // 分享到微博
+        WeixinJSBridge.on('menu:share:weibo', function(argv){
+            shareWeibo();
+            });
+        }, false);*/
+//微信分享用 结束
---
+++ js/index.js
@@ -0,0 +1,216 @@
+//    Zepto(function($){
+$(function(){
+    newHeight = $(window).height();//得到屏幕的高
+    newWidth = $(window).width();//得到屏幕的宽
+    $(".win-height").css('height',newHeight);
+    $(".win-width").css('width',newWidth);
+
+    /*
+     浏览器事件类型
+     'transition':'transitionend',
+     'OTransition':'oTransitionEnd',
+     'MozTransition':'transitionend',
+     'WebkitTransition':'webkitTransitionEnd',
+     'MsTransition':'msTransitionEnd'
+     */
+    var jumpBtn = $(".jump-btn");
+
+    //获取当前浏览器前缀
+    var getVendorPrefix = function() {
+        var body = document.body || document.documentElement,
+            style = body.style,
+            vendor = ['webkit', 'khtml', 'Moz', 'ms', 'o'],
+            i = 0;
+
+        while (i < vendor.length) {
+            // 此处进行判断是否有对应的内核前缀
+            if (typeof style[vendor[i] + 'Transition'] === 'string') {
+                return vendor[i];
+            }
+            i++;
+        }
+    };
+    var vendorPrefix = getVendorPrefix();
+    //构建当前浏览器对应的事件名称
+    var transitionend = (vendorPrefix==""|| vendorPrefix=="Moz")?"transitionend":vendorPrefix+"TransitionEnd";
+
+    //构建当前浏览器对应的动画状态名称
+    var animationPlayState = vendorPrefix==""?"animationPlayState":vendorPrefix+"AnimationPlayState";
+
+    var powerWarp; //能得到的最大能量
+    var powerVal;//能量值
+    var observerNumber;//高度监听
+    var jumpNumber;//目标高度数字
+    var showJumpNumber=0;//高度数字
+    var effectStatus=[false,false];//0位小人动画结束状态标示，1位点击弹框中的按钮状态
+
+    //设置高度数字
+    var setJumpNumber = function(delayNumber){
+        observerNumber = setTimeout(function(){
+//            console.log("showJumpNumber:",showJumpNumber,"jumpNumber:",jumpNumber);
+            if(showJumpNumber!=jumpNumber){
+                showJumpNumber+=100;
+                $(".jump_num").text(showJumpNumber);
+                setJumpNumber(30);
+            }else{
+                clearTimeout(observerNumber);
+                observerNumber = null;
+            }
+        },delayNumber||400);
+    };
+
+    //清空高度数字
+    var clearJumpNumber = function () {
+        showJumpNumber=0
+    };
+    //禁用jump按钮
+    var disableJumpBtn = function(){
+        $(".disabled").css("display","block");
+    };
+    //启用jump按钮
+    var ableJumpBtn = function(){
+        $(".disabled").css("display","none");
+    };
+    //停止能量槽聚集能量
+    var stopPower = function(power){
+//            power.css(animationPlayState,"paused");
+        $("#stop").attr("checked","checked");
+        //警用jump按钮
+        disableJumpBtn();
+    };
+    //重置能量槽
+    var resetPower = function(flag){
+//            $(".power").css(animationPlayState,"running");
+        if(effectStatus[0] && effectStatus[1]){//两个状态都完成后重置能量槽
+            effectStatus[0] = false;
+            effectStatus[1] = false;
+            $("#reset").attr("checked","checked");
+//            $("#start").attr("checked","checked");
+            setTimeout(function(){
+                if(flag){
+                    $(".h_meter").eq(0).text(0);
+                }else{
+                    $(".h_meter").eq(1).text(0);
+                }
+                $(".jump_num").text(0);
+                $("#start").attr("checked","checked");
+            },100);
+        }
+    };
+    //获取能量值
+    var getPower = function(){
+        var power = $(".power");
+        stopPower(power);
+        var powerMaxVal = power.get(0).offsetHeight;
+        powerVal = ((powerMaxVal-power.get(0).offsetTop)/powerMaxVal).toFixed(2)*100;
+        jumpNumber = Math.round(10000*powerVal/1000)*10;
+        data["jumpNumber"] = jumpNumber;
+//        console.log("powerMaxVal.....powerMaxVal....:",powerVal+"%",jumpNumber);
+    };
+    //people起跳动画回调方法
+    var jumbCallBack = function(){
+        getPower();
+        clearJumpNumber();
+        setJumpNumber();
+        var showNext = function(){
+            var people =  $($(".page3_peo .show").get(0));
+            people.toggleClass("show");
+            people.next().toggleClass("show");
+        };
+        var step2 = function(){
+            showNext();
+            $(".page3_peo").css("bottom","55%");
+        };
+        var step3 = function(){
+            showNext();
+        };
+        var step4 = function(){
+            showNext();
+        };
+        showNext();
+        setTimeout(step2,400);
+        setTimeout(step3,800);
+        setTimeout(step4,1000);
+    };
+    //people降落动画回调方法
+    var dropCallBack = function(peo){
+        var showPrev = function(flag){
+            var curPeople =  $(peo.find(".show").get(0));
+            curPeople.toggleClass("show");
+            if(flag){
+                curPeople.prev().prev().prev().toggleClass("show");
+            }else{
+                curPeople.prev().toggleClass("show");
+            }
+        };
+        var step2 = function(){
+            showPrev(true);
+        };
+        var step3 = function(){
+            showPrev();
+            ableJumpBtn();
+            effectStatus[0]=true;
+            resetPower();
+        };
+        setTimeout(step2,600);
+        setTimeout(step3,1000);
+    };
+//    jumpBtn.on("click",jumbCallBack);
+      jumpBtn.on("touchstart",jumbCallBack);
+
+    //初始时底边距值
+    var peoBottom = $(".page3_peo").css("bottom");
+
+    //people落地方法
+    var peoDrop = function(){
+        var peo = $(".page3_peo");
+        peo.css("bottom","10%");
+        dropCallBack(peo);
+    };
+    //people容器动画结束监听事件
+    var peoTransitionEndEvent = function(event){
+        var bottom = $(event.target).css("bottom");
+        if(bottom == peoBottom){
+            return;
+        }
+        if(powerVal>40){
+            $(".page3_move").css("bottom","-"+powerVal+"%");
+
+            //隐藏影子
+            $(".page3_bg_disc").css("display","none");
+        }else{
+            showDialog(jumpNumber>6999);
+            peoDrop();
+        }
+    };
+    $(".page3_peo").on(transitionend,peoTransitionEndEvent);
+    //背景动画结束监听事件
+    var bgTransitionEndEvent = function(event){
+        var moveBg = $(".page3_move");
+        if(moveBg.css("bottom")!="0px"){
+            showDialog(jumpNumber>6999);
+            moveBg.css("bottom","0px");
+        }else{
+            //显示影子
+            $(".page3_bg_disc").css("display","block");
+            peoDrop();
+        }
+    };
+    //弹框显示结果
+    var showDialog = function(flag){
+        data["status"] = flag;
+        if(flag){
+            $(".h_meter").eq(0).text(jumpNumber);
+            $(".success_dialog").css("display","block");
+        }else{
+            $(".h_meter").eq(1).text(jumpNumber);
+            $(".error_dialog").css("display","block");
+        }
+    };
+    $(".page3_move").on(transitionend,bgTransitionEndEvent);
+    $(".reset").on("click",function(){
+        $(".page3_hide").css("display","none");
+        effectStatus[1]=true;
+        resetPower(jumpNumber>6999);
+    });
+});
